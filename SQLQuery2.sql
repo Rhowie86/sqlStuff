692f11d318d8ecfc87fb5d08b6371fd2dec9ead6
Rhowie86 <ronaldnhowie@gmail.com>
2021-03-10 17:58:26 -0600

    poem stuff

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -7,11 +7,42 @@
 
 --SELECT Album.Title, Song.Title FROM Album Left Join Song on Album.Id = Song.AlbumId WHERE Song.AlbumId IS Null;
 
-SELECT * FROM Album
+--SELECT * FROM Album
 
---INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Tears For Fears', 1980);
 
---INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('Songs From The Big Chair', 2/25/1985, 4152, 'Phonogram', 28, 2); 
+--INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('David Bowie', 1980);
+
+--INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('Aladdinsane', '3/09/2021', 4152, 'Idonutknow', 29, 2); 
+
+--SELECT al.Title as AlbumTitle, s.Title as SongTitle, a.ArtistName
+--FROM Album al
+--LEFT JOIN Song s ON s.AlbumId = al.Id
+--LEFT JOIN Artist a ON al.ArtistId = a.Id
+--WHERE al.ArtistId = 28;
+
+--select count (s.id) as SongCount, al.Title as AlbumTitle, ar.ArtistName
+	--from song s
+	--left join Album al on s.AlbumId = al.Id
+	--left join Artist ar on al.ArtistId = ar.Id
+--group by al.Title, ar.ArtistName
+
+
+--select count(distinct al.label), ar.ArtistName
+	--from Artist ar JOIN Album al on ar.id = al.ArtistId
+--group by ar.ArtistName
+	--having count(distinct al.label) > 1
+
+
+select s.Title, s.SongLength
+	from song s
+	where s.SongLength = (
+		select max (s.SongLength)
+		  from song s
+		);
+
+select top 2 s.Title, s.SongLength 
+	from song s
+order by s.SongLength desc;
 
 
 
---
+++ poki.sql
@@ -0,0 +1,124 @@
+﻿--1. What grades are there?
+
+select *
+from Grade;
+
+--2. What emotions may be associated with a poem?
+
+select *
+from Emotion;
+
+--3. How many poems are in the database?
+
+select count (p.Id) as NumberOfPoems
+from Poem p;
+
+--4. Sort authors alphabetically by name. What are the names of the top 76 authors?
+
+select top 76 (a.Name) as AuthorName
+from Author a
+	order by a.Name asc;
+
+
+--5. Starting with the above query, add the grade of each of the authors.
+
+select top 76 (a.Name) as AuthorName, g.Name as AuthorsGrade
+from Author a
+LEFT JOIN Grade g ON a.GradeId = g.Id
+	order by a.Name asc;
+
+--6. Starting with the above query, add the recorded gender of each of the authors.
+
+select top 76 (a.Name) as AuthorName, g.Name as AuthorsGrade, gen.Name as AuthorsGender
+from Author a
+LEFT JOIN Grade g ON a.GradeId = g.Id
+LEFT JOIN Gender gen ON a.GenderId = gen.Id
+	order by a.Name asc;
+
+--7. What is the total number of words in all poems in the database?
+
+select count (p.WordCount) as TotalWords
+	from Poem p;
+
+-- 8. Which poem has the fewest characters?
+
+select p.Title as PoemName, p.CharCount as CharCount
+from poem p
+	where p.CharCount = (
+	select min (p.CharCount)
+	from poem p);
+
+--9. How many authors are in the third grade?
+
+select count (a.GradeId) as TotalThirdGraders
+	from Author a
+	Where a.GradeId = 3;
+
+--10. How many authors are in the first, second or third grades?
+
+select count (a.GradeId) as TotalFirstGraders
+	from Author a
+	where a.gradeId = 1;
+		select count (a.GradeId) as TotalSecondGraders
+		from Author a
+		where a.GradeId = 2;
+			select count (a.GradeId) as TotalThirdGraders
+			from Author a
+			where a.GradeId = 3;
+
+--11. What is the total number of poems written by fourth graders?
+
+select count (p.id) as totalPoemsByFourthGraders
+ from Poem p
+	JOIN Author a ON p.AuthorId = a.Id
+	JOIN Grade g ON a.GradeId = g.Id
+	where g.Name = '4th Grade';
+
+--12. How many poems are there per grade
+
+select count(p.id) as totalPoemsByGrade
+	from Poem p
+		JOIN author a on p.authorid = a.id
+		JOIN Grade g on a.GradeId = g.id
+		group by g.id
+
+--13. How many authors are in each grade? (Order your results by grade starting with 1st Grade)
+
+select count (a.id) as TotalAuthorsByGrade
+	from Author a
+		join Grade g on a.GradeId = g.id
+	group by g.id
+
+--14. What is the title of the poem that has the most words?
+
+select p.Title
+	from Poem p
+where p.WordCount = (
+	select max (p.WordCount)
+		from Poem p
+		);
+
+--15. Which author(s) have the most poems? (Remember authors can have the same name.)
+
+select count(distinct p.AuthorId) as NumberOfPoems, a.Name
+	from Poem p JOIN Author a on p.AuthorId = a.Id
+group by a.Name
+	having count(distinct p.AuthorId) > 1
+
+
+--16. How many poems have an emotion of sadness?
+
+select count (p.Id)
+	from Poem p JOIN PoemEmotion emo on p.Id = emo.PoemId
+	where emo.EmotionId = 3;
+
+--17. How many poems are not associated with any emotion?
+
+select count (p.Id)
+	from Poem p 
+	JOIN PoemEmotion pemo on p.Id = pemo.PoemId
+	JOIN Emotion emo on pemo.Id = emo.Id
+	where emo.Name = NULL
+	
+	
+
